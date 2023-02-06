5a4048b74d735110393c55fb2c25ff3d01b753ac
Lukman Gbolahan <99412178+Gbollly@users.noreply.github.com>
2023-02-06 10:03:02 +0100

    Delete loopstudios-landing-page-main directory

--- loopstudios-landing-page-main/loop.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-document.querySelector(".menu-hamburger").addEventListener("click", function () {
-    document.querySelector(".nav-mobile").classList.remove("hiddennav");
-});
-
-document.querySelector(".closeMenu").addEventListener("click", function(){
-    document.querySelector(".nav-mobile").classList.add("hiddennav");
-});
